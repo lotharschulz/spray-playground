d989b6695d05c0d63733ef274edce1a401b5ce5e
Lothar Schulz <lothar.sch@gmx.de>
2016-04-25 23:12:49 +0200

    post tests

--- src/main/scala/info/lotharschulz/MyService.scala
+++ src/main/scala/info/lotharschulz/MyService.scala
@@ -2,52 +2,38 @@ package info.lotharschulz
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.Props
+import spray.json.DefaultJsonProtocol
+
 import akka.util.Timeout
-import info.lotharschulz.book.BookActor
-import org.json4s.{Formats, DefaultFormats}
-import spray.http.StatusCodes._
-import spray.httpx.Json4sSupport
+import spray.http._
+import spray.httpx.SprayJsonSupport
 import spray.routing._
-import akka.pattern.ask
 
-object Json4sProtocol extends Json4sSupport {
-  implicit def json4sFormats: Formats = DefaultFormats
+case class Book(title: String)
+
+object Json4sProtocol extends DefaultJsonProtocol with SprayJsonSupport {
+  implicit val PortfolioFormats = jsonFormat1(Book)
 }
-
-case class Entity(payload: String)
 
 trait MyService extends HttpService {
   import Json4sProtocol._
-  import BookActor._
 
   implicit def executionContext = actorRefFactory.dispatcher
   implicit val timeout = Timeout.apply(5L, TimeUnit.SECONDS)
 
-  val bookworker = actorRefFactory.actorOf(Props[BookActor], "worker")
-
   val route = {
     path("book") {
       get {
-        complete(List(Entity("smth"), Entity("smth more")))
+        complete(List(Book("smth"), Book("smth more")))
       } ~
         post {
-          respondWithStatus(Created) {
-            entity(as[Entity]) { someObject =>
-              doCreate(someObject)
+          respondWithMediaType(MediaType.custom("application/json")) {
+            formFields('title.as[String]) { (title) =>
+              complete {"{\"book\": { \"title\":\"%s\"]} }" format (title)
+              }
             }
           }
         }
     }
   }
-
-  def doCreate[T](ent: Entity) = {
-    complete {
-      (bookworker ? Create(ent))
-        .mapTo[BookActor]
-        .map(result => s"I got a response: ${result}")
-        .recover { case _ => "error" }
-    }
-  }
-
 }
--- src/test/scala/info/lotharschulz/MyServiceSpec.scala
+++ src/test/scala/info/lotharschulz/MyServiceSpec.scala
@@ -1,8 +1,9 @@
 package info.lotharschulz
 
 import org.specs2.mutable.Specification
-import spray.http.{MediaTypes, StatusCodes}
+import spray.http._
 import spray.testkit.Specs2RouteTest
+import spray.http.StatusCodes.NotFound
 
 class MyServiceSpec extends Specification with Specs2RouteTest with MyService  {
   def actorRefFactory = system
@@ -10,12 +11,30 @@ class MyServiceSpec extends Specification with Specs2RouteTest with MyService  {
     "return a json list" in {
       import Json4sProtocol._
       Get("/book") ~> route ~> check {
-        val res = responseAs[List[Entity]]
+        val res = responseAs[List[Book]]
         res.size === 2
-        res(0).payload must be equalTo "smth"
-        res(1).payload === "smth more"
+        res(0).title must be equalTo "smth"
+        res(1).title === "smth more"
         status === StatusCodes.OK
         mediaType === MediaTypes.`application/json`
+      }
+    }
+  }
+
+  "MyService" should {
+    "create a new book" in {
+      Post("/book", FormData(Map("title" -> "icke"))) ~> route ~> check {
+        //status should be equalTo Created
+        status === StatusCodes.OK
+        mediaType === MediaTypes.`application/json`
+      }
+    }
+  }
+
+  "MyService" should {
+    "not support PUT verb" in {
+      Put() ~> sealRoute(route) ~> check {
+        status === NotFound
       }
     }
   }
